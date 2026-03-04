2aac288777690da11f746a45cbe44bd7ecb65f33
varun thakur <vtthakurvarun@gmail.com>
2026-03-04 14:29:34 +0530

    learning api

--- 1_server using express/1_basic.js
+++ 1_server using express/1_basic.js
@@ -1,10 +1,10 @@
 //first do nbm init to initialize server using express
-const express=require("express")
-const app=express()
-app.get("/",(req, res)=>{
+const express=require("express") //doing require the server 
+const app=express() //creating the server instance(A specific copy of something created from a class or template.)
+app.get("/",(req, res)=>{  //the request we get form user and the response we send form server form res obj
     res.send("hello world")
 })
 app.get("/about",(req,res)=>{
     res.send("about page")
 })
-app.listen(3000)
+app.listen(3000) //use to start server //3000 is the port number
---
+++ 1_server using express/2_api.js
@@ -0,0 +1,8 @@
+//api is used to get data form the another site or we can say set of rules that allow to exchange the data and function between them
+//rest api is the type of api standard http method get post put delete
+
+//get => get the data from server
+//post +> post data to server
+//patch =>data is already and update the data 
+//delete=>delete data from server
+
