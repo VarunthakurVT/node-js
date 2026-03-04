86a912c1022763d600d83897d3b65cb0d59d2610
varun thakur <vtthakurvarun@gmail.com>
2026-03-04 12:50:49 +0530

    learning the express

---
+++ 1_server using express/1_basic.js
@@ -0,0 +1,10 @@
+//first do nbm init to initialize server using express
+const express=require("express")
+const app=express()
+app.get("/",(req, res)=>{
+    res.send("hello world")
+})
+app.get("/about",(req,res)=>{
+    res.send("about page")
+})
+app.listen(3000)
