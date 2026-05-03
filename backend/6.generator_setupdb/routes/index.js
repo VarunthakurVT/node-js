55fdcb9a1a557c6a53660e3b01f19c24265f875e
varun thakur <vtthakurvarun@gmail.com>
2026-05-03 19:24:44 +0530

    lerning the backend stuff like the middleware and database and the definatio of the session

--- backend/6.generator_setupdb/routes/index.js
+++ backend/6.generator_setupdb/routes/index.js
@@ -5,7 +5,10 @@ const userModel = require('./users');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  
+  
 });
+
 
 router.get('/create',async function(req,res){
   await userModel.create({
@@ -15,4 +18,30 @@ router.get('/create',async function(req,res){
   })
   res.send("user is created")
 })
+router.get("/allusers",async function(req,res){
+  let allUsers=await userModel.findOne({username: "varunthakur"})
+  res.send(allUsers);
+})
+router.get("/delete",async function(req,res){
+  let deletedUser=await userModel.findOneAndDelete({
+    username:"Varunthakur"
+  })
+  res.send(deletedUser);
+})
+router.get("/find",async function(req,res){
+  let allUsers=await userModel.find()
+  res.send(allUsers)
+})
+router.get("/ban",function(req,res){
+  req.session.koibhikam="hello";
+  req.session.banned=true;
+})
+router.get("/checkban",function(req,res){
+  console.log(req.session);
+  res.send("check the console")
+  if(req.session.banned==true){
+    res.send("you are banned ")
+  }
+})
+
 module.exports = router;
